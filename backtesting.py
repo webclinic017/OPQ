2a31b5482fec8f28aa7964fff7f113e3679b60b8
BIOWP <bioweapon@126.com>
2019-03-23 11:50:40 +0800

    UI Improved
    1. More flexible to run backtesting using Command Line Prompt

--- backtesting.py
+++ backtesting.py
@@ -4,6 +4,7 @@ Backtest the strategies.
 '''
 
 import os
+import argparse
 
 import pandas as pd
 import numpy as np
@@ -25,6 +26,20 @@ def print_order(orders, stock_data, indent=1):
             quantity = -quantity
             print('\t' * indent + f"Sold/shorted {quantity} shares of {code} @ {price}")
 
+def print_config(config):
+    '''
+    Print the key configurations for backtesting.
+    '''
+
+    backtesting_start = config.get('BACKTESTING_START', '?')
+    backtesting_end = config.get('BACKTESTING_END', '?')
+    exit_threshold = config.get('EXIT_THRESHOLD', '?')
+    enter_threshold = config.get('ENTER_THRESHOLD', '?')
+    stop_threshold = config.get('STOP_THRESHOLD', '?')
+
+    print(f"Back-testing period=from {backtesting_start} to {backtesting_end}")
+    print(f"enter={enter_threshold}, stop={stop_threshold}, exit={exit_threshold}")
+    
     
 
 def cash_change(orders, stock_data, tx_cost_per_share=0, tx_cost_per_dollar=0):
@@ -142,10 +157,11 @@ def evaluate(strategy, config):
     })
     final_return = daily_tnw[-1] / initial_cash - 1
     daily_tnw_aug = [initial_cash] + daily_tnw
+    ret_std = df_metrics['ret'].std()
     performance_metrics = {
         "Final Return": final_return,
-        "Volatility": df_metrics['ret'].std(),
-        "Sharpe Ratio": (final_return - df_metrics['ret'].mean()) / df_metrics['ret'].std(),
+        "Volatility": ret_std,
+        "Sharpe Ratio": (final_return - df_metrics['ret'].mean()) / ret_std if ret_std > 0 else np.nan,
         "Up Percentage": len([r for r in daily_return if r > 0]) / len(daily_return),
         "Max Drawdown": (min(daily_tnw_aug) - max(daily_tnw_aug)) / max(daily_tnw_aug),
         "Skewness": df_metrics['ret'].skew(),
@@ -173,85 +189,141 @@ def select_pairs_bulk():
     print("Exported", "CoInt")
 
 
-def evaluate_bulk():
 
-    config = util.load_config()
-
-    num_pairs_limit = int(config.get("NUM_PAIRS_LIMIT", 0))
-
-    exit_threshold = float(config["EXIT_THRESHOLD"])
-    enter_threshold = float(config["ENTER_THRESHOLD"])
-    stop_threshold = float(config["STOP_THRESHOLD"])
-    
-    backtesting_start = config['BACKTESTING_START']
-    backtesting_end = config['BACKTESTING_END']
-    
-    thresholds = {
-        'enter': enter_threshold,
-        'stop': stop_threshold,
-        'exit': exit_threshold
-    }
-
-    print(f"Back testing period = from {backtesting_start} to {backtesting_end}")
-    print(f"num_pairs_limit={num_pairs_limit}, enter={enter_threshold}, stop={stop_threshold}, exit={exit_threshold}")
-
-    for fname in os.listdir('Pairs'):
-        print("\nEvaluating", fname, "...")
-        pairs = PairTradeStrategy.load_pairs(os.path.join('Pairs', fname))
-        if num_pairs_limit:
-            pairs = pairs[:num_pairs_limit]
-        strategy = PairTradeStrategy(thresholds, pairs)
-        results = evaluate(strategy, config)
-        for metric, value in results.items():
-            print(metric, round(value, 5), sep='\t')
-
-
-
-def evaluate_by_num_of_pairs(filename, lower, upper):
+def evaluate_cumulative_pairs(pairs, config, lower=None, upper=None):
     '''
-    Evaluate a portfolio by various number of pairs.
+    Evaluate pairs cumulatively of a portfolio.
     '''
 
-    config = util.load_config()
-    exit_threshold = float(config["EXIT_THRESHOLD"])
-    enter_threshold = float(config["ENTER_THRESHOLD"])
-    stop_threshold = float(config["STOP_THRESHOLD"])
-    backtesting_start = config['BACKTESTING_START']
-    backtesting_end = config['BACKTESTING_END']
-    
+   
+
     thresholds = {
-        'enter': enter_threshold,
-        'stop': stop_threshold,
-        'exit': exit_threshold
+        'enter': float(config["ENTER_THRESHOLD"]),
+        'stop': float(config["STOP_THRESHOLD"]),
+        'exit': float(config["EXIT_THRESHOLD"])
     }
-
-    print(f"Back testing period = from {backtesting_start} to {backtesting_end}")
-    print(f"enter={enter_threshold}, stop={stop_threshold}, exit={exit_threshold}")
-    print("Evaluating", filename, f"with number of pairs ranging from {lower} to {upper}")
-    print()
+    if lower is None:
+        lower = [1, 1]
+    lower_start = int(lower[0])
+    lower_end = int(lower[1]) if len(lower) > 1 else lower_start
+    if upper is None:
+        upper = [len(pairs), len(pairs)]
+    upper_start = int(upper[0])
+    upper_end = int(upper[1]) if len(upper) > 1 else upper_start
     
-    pairs_original = PairTradeStrategy.load_pairs(filename)
-    metric_columns = None
-    for num_pairs_limit in range(lower, upper + 1):
-        pairs = pairs_original[:num_pairs_limit]
-        strategy = PairTradeStrategy(thresholds, pairs)
-        results = evaluate(strategy, config)
-        if metric_columns is None:
-            metric_columns = list(results.keys())
-            print('\t'.join(['Num_pairs'] + metric_columns))
-        print(num_pairs_limit, end='\t')
-        print('\t'.join([str(round(results[metric], 5)) for metric in metric_columns]))
+    pairs_original = pairs
+    columns = None
+    
+    for s in range(lower_start - 1, lower_end):
+        for e in range(upper_start, upper_end + 1):
+            pairs = pairs_original[s : e]
+            strategy = PairTradeStrategy(thresholds, pairs)
+            results = evaluate(strategy, config)
+            if columns is None:
+                columns = ['Start Pair', 'End Pair'] + list(results.keys())
+                print(*columns, sep='\t')
+            results['Start Pair'] = s
+            results['End Pair'] = e
+            print(*[results[c] if type(results[c]) is str else round(results[c], 5) for c in columns], sep='\t')
 
-  
+
+
+def evaluate_individual_pairs(pairs, config, lower=None, upper=None):
+    '''
+    Evaluate pairs individually of a portfolio.
+    '''
+
+    thresholds = {
+        'enter': float(config["ENTER_THRESHOLD"]),
+        'stop': float(config["STOP_THRESHOLD"]),
+        'exit': float(config["EXIT_THRESHOLD"])
+    }
+    if lower is None:
+        lower = [1]
+    lower = int(lower[0])
+    if upper is None:
+        upper = [len(pairs)]
+    upper = int(upper[0])
+    columns = None
+    for pair in pairs[lower - 1 : upper]:
+        strategy = PairTradeStrategy(thresholds, [pair])
+        results = evaluate(strategy, config)
+        if columns is None:
+            columns = ['Stock_1', 'Stock_2'] + list(results.keys())
+            print(*columns, sep='\t')
+        results['Stock_1'] = pair['Stock_1']
+        results['Stock_2'] = pair['Stock_2']
+        print(*[results[c] if type(results[c]) is str else round(results[c], 5) for c in columns], sep='\t')
+        
+
 
 if __name__ == "__main__":
-    # evaluate_by_num_of_pairs("Pairs/top_40_pairs_by_CoInt.csv", 10, 30)
-    evaluate_bulk()
+
+    config = util.load_config()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', dest="in_file",
+                        default=None, help="A csv file that specifies a list of stock pairs. Could also be a folder of csv files.")
+    parser.add_argument('-d', '--directory', '--folder', dest="in_directory",
+                        default="Pairs/", help="A folder of csv files, each specifying a list of stock pairs.")
+    parser.add_argument('-i', '--individual', dest="indiv",
+                        default=False, action='store_true', help="Evaluate pairs individually.")
+    parser.add_argument('-p', '--pair', dest="pairs",
+                        default=None, action='append', nargs='+', help="A specific pair (Stock_1, Stock_2[, beta]) to evaluate.")
+    parser.add_argument('-l', '--lower', dest="lower",
+                        default=None, nargs='+', help="The lower bound of number of pairs or the starting pair indices.")
+    parser.add_argument('-u', '--upper', dest="upper",
+                        default=None, nargs='+', help="The upper bound of number of pairs or the ending pair indices.")
+    config_params = [
+        "BACKTESTING_START",
+        "BACKTESTING_END",
+        "EXIT_THRESHOLD",
+        "ENTER_THRESHOLD",
+        "STOP_THRESHOLD",
+        "TX_COST_PER_SHARE",
+        "TX_COST_PER_DOLLAR"
+    ]
+    for param in config_params:
+        parser.add_argument(f'--{param}', dest=param,
+                            default=None, help="Overwrite this parameter in config.txt")
+    args = parser.parse_args()
+    
+    for param in config_params:
+        if param in config and getattr(args, param) is not None:
+            config[param] = getattr(args, param)
+    print_config(config)
+    print()
+
+    if args.in_file:
+        pairs = PairTradeStrategy.load_pairs(args.in_file)
+        if args.indiv:
+            evaluate_individual_pairs(pairs, config, args.lower, args.upper)
+        else:
+            evaluate_cumulative_pairs(pairs, config, args.lower, args.upper)
+    elif args.pairs:
+        pairs = []
+        for pair in args.pairs:
+            pairs.append({
+                'Stock_1': pair[0],
+                'Stock_2': pair[1],
+                'beta': float(pair[2] if len(pair) >= 3 else 1)
+            })
+        if args.indiv:
+            evaluate_individual_pairs(pairs, config, args.lower, args.upper)
+        else:
+            evaluate_cumulative_pairs(pairs, config, args.lower, args.upper)
+    else:
+        for fname in os.listdir(args.in_directory):
+            print("File:", fname)
+            fname = os.path.join(args.in_directory, fname)
+            pairs = PairTradeStrategy.load_pairs(fname)
+            if args.indiv:
+                evaluate_individual_pairs(pairs, config, args.lower, args.upper)
+            else:
+                evaluate_cumulative_pairs(pairs, config, args.lower, args.upper)
+            print()
+
     input("\nDone")
-
-
-
-
 
 
 
